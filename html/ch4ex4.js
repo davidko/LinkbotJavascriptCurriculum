7dcf8bbd9e73a7b24dc7b16afbbce46d7e9a53c2
David Ko <david.dk.ko@gmail.com>
2014-04-04 16:25:39 -0700

    Removed Motor2 text entries, fixed GET param bug

--- html/ch4ex4.js
+++ html/ch4ex4.js
@@ -3,10 +3,9 @@ $( function() {
 var r = $('.ch4ex4');
 r.find('.tryNow').click( function(obj) {
   var j1text = r.find('.j1').val();
-  var j2text = r.find('.j2').val();
   var j3text = r.find('.j3').val();
 	var j1 = parseFloat(j1text);
-	var j2 = parseFloat(j2text);
+	var j2 = 0;
 	var j3 = parseFloat(j3text);
 	if ( (j1 > 0) && (j3 < 0) && (j1 == (j3*-1)) ) {
 		r.find('.checkBox').attr("src", "images/check.svg");
--- html/ch4ex5.js
+++ html/ch4ex5.js
@@ -4,10 +4,9 @@ var r = $('.ch4ex5');
 
 r.find('.tryNow').click( function(obj) {
   var j1text = r.find('.j1').val();
-  var j2text = r.find('.j2').val();
   var j3text = r.find('.j3').val();
 	var j1 = parseFloat(j1text);
-	var j2 = parseFloat(j2text);
+	var j2 = 0;
 	var j3 = parseFloat(j3text);
 	if ( (j1 < 0) && (j3 < 0) && (j1 == j3) ) {
 		r.find('.checkBox').attr("src", "images/check.svg");

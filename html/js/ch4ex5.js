058858d8a2b4e2ab1f237974cb0861c6838d6de8
David Ko <david.dk.ko@gmail.com>
2014-04-07 21:30:20 -0700

    Challenge code snippets can now be expanded

--- html/js/ch4ex5.js
+++ html/js/ch4ex5.js
@@ -1,18 +1,21 @@
 $( function() {
-var showWholeCode = false;
-var chapterClassName = '.ch4ex3';
 
+var r = $('.ch4ex5');
+var showWholeCode = false;
 var j1text = '0';
 var j3text = '0';
 
-var r = $('.ch4ex3')
-
 r.find('.tryNow').click( function(obj) {
-	var robotID = GetRobotId();
-	var bot = Linkbots.connect(robotID);
 	var j1 = parseFloat(j1text);
 	var j2 = 0;
 	var j3 = parseFloat(j3text);
+	if ( (j1 < 0) && (j3 < 0) && (j1 == j3) ) {
+		r.find('.checkBox').attr("src", "images/check.svg");
+	} else {
+		r.find('.checkBox').attr("src", "images/cross.svg");
+	}
+	var robotID = GetRobotId();
+	var bot = Linkbots.connect(robotID);
 	bot.move(j1, j2, j3);
 });
 
@@ -21,6 +24,8 @@ r.find('.stop').click( function(obj) {
 	var bot = Linkbots.connect(robotID);
 	bot.stop();
 });
+
+AddRobotToGetParams(GetRobotId());
 
 r.find('input.j1text').keyup( function() {
   r.find('input.j1text').val( $(this).val() );
@@ -31,8 +36,6 @@ r.find('input.j3text').keyup( function() {
   r.find('input.j3text').val( $(this).val() );
   j3text = $(this).val();
 });
-
-AddRobotToGetParams(GetRobotId());
 
 r.find('.expandProgram').click( function(obj) {
   if(showWholeCode) {

25ba150584b1fdf67cdcb9354655e86da33c785e
Harris Hancock <vortrab@gmail.com>
2014-04-09 16:50:23 -0700

    Modify show/hide "abridged code" code (WIP)

--- html/js/ch4ex1.js
+++ html/js/ch4ex1.js
@@ -5,28 +5,30 @@ var robotID = GetRobotId();
 if(robotID == undefined) {
   robotID = 'ABCD';
 }
+else {
+  $('.unknown-robot-id-comment', chapterClassName).text('');
+}
 
 $(chapterClassName).find('.tryNow').click( function(obj) {
-
   var bot = Linkbots.connect(robotID);
   bot.move(360, 0, 0);
 });
 
 AddRobotToGetParams(robotID);
+$('.robotID', chapterClassName).text(GetRobotId());
 
-$(chapterClassName).find('.expandProgram').click( function(obj) {
-  if(showWholeCode) {
-    $(chapterClassName).find('.unabridgedCode').attr('style', 'display: none;');
-    $(chapterClassName).find('.abridgedCode').attr('style', 'visibility: visible;');
-    $(chapterClassName).find('.expandProgramText').text('Show Unabridged Program');
-    showWholeCode = false;
-  } else {
-    $(chapterClassName).find('.programlisting').find('.robotID').text( GetRobotId() );
-    $(chapterClassName).find('.unabridgedCode').attr('style', 'visibility: visible;');
-    $(chapterClassName).find('.abridgedCode').attr('style', 'display: none;');
-    $(chapterClassName).find('.expandProgramText').text('Show Abridged Program');
-    showWholeCode = true;
-  }
+var nextRotateAngle = 180;
+
+$('.expand-program', chapterClassName).click(function (event) {
+    // Suppress addition of this navigation event to the browser's history, so
+    // the Back button isn't screwed up.
+    event.preventDefault();
+
+    $('pre.hidden', chapterClassName).slideToggle();
+
+    // Flip the hider tab image
+    $('img', this).css('transform', 'rotate(' + nextRotateAngle + 'deg)');
+    nextRotateAngle = (nextRotateAngle + 180) % 360;
 });
 
 });
